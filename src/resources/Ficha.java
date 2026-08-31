895cea750415f22bfef6b1a8677219e737497d9a
fromkas1 <francogonzaleztato@gmail.com>
2026-08-31 11:57:19 -0300

    Clase ficha agregada

---
+++ src/resources/Ficha.java
@@ -0,0 +1,39 @@
+package resources;
+
+public class Ficha {
+	private int x;
+	private int y;
+	private int valor;
+	
+	public Ficha(int x, int y, int valor) {
+		this.x = x;
+		this.y = y;
+		this.valor = valor;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
