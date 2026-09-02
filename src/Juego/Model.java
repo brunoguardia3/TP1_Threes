8a374fa50304e3540ef15b8100bffd8c093ad416
brunoguardia3 <brunoguardia3@gmail.com>
2026-09-02 15:27:24 -0300

    add isJuegoTerminado()

--- src/Juego/Model.java
+++ src/Juego/Model.java
@@ -1,5 +1,12 @@
 package Juego;
 
 public class Model {
-
+//	new Tablero tablero = new Tablero();
+	int puntuación = 0;
+	boolean juegoTerminado = false;
+	
+	public void isJuegoTerminado() {
+		if (tablero.obtenerElementos == 14) {
+			juegoTerminado = true;
+		}
 }
