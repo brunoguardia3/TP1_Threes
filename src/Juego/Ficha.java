1a4a7dd76c01e3907beedbe66408d8d2cd5872ee
fromkas1 <francogonzaleztato@gmail.com>
2026-09-03 12:41:36 -0300

    fix errores

--- src/Juego/Ficha.java
+++ src/Juego/Ficha.java
@@ -1,5 +1,19 @@
 package Juego;
 
 public class Ficha {
+	private int valor;
+	
+	public Ficha(int valor) {
+		this.valor = valor;
+	}
 
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
 }
