386d7922cdba216136aa38d96213bd3010f05053
Daniela Belén Chesko <77978678+DanielaChesko@users.noreply.github.com>
2026-09-05 02:47:29 -0300

    metodos de Tablero y correccion y creacion de metodos en Model
    Hice todos los métodos de Tablero.java menos el de agregarFicha() -pendiente-.
    Después hice unas correcciones de nombres en Model.java, le puse constructor para inicializar el valor random de la siguiente ficha, agregue el método para generar ese valor random y el metodo para poder traer ese valor para Tablero.java

--- src/juego/Model.java
+++ src/juego/Model.java
@@ -1,20 +1,44 @@
 package juego;
 
+import java.util.Random;
+
 public class Model {
 	Tablero tablero = new Tablero();
 	int puntuación = 0;
 	boolean juegoTerminado = false;
+	
+	private int siguienteValor;
+	
+	public Model() {
+		this.siguienteValor = generarFichaAleatoria();
+	}
+	
+	public int generarFichaAleatoria() {
+		Random random = new Random();
+		
+		int[] posiblesValoresFichas = {1,2,3};
+		
+		int indice = random.nextInt(posiblesValoresFichas.length);
+		
+		int fichaAleatoria = posiblesValoresFichas[indice];
+		
+		return fichaAleatoria;
+	}
+	
+	public int getSiguientValor() {
+		return siguienteValor;
+	}
 
 	public void isJuegoTerminado() {
-		if(tablero.cantidadDeFichas() < 16) {
+		if(tablero.cantidadDeFichasPresentes() < 16) {
 			juegoTerminado = false;
 			return;
 		}
 		boolean sinCombinacionesVerticales = true;
 		boolean sinCombinacionesHorizontales = true;
 
-		for (int i = 0; i < tablero.obtenerLargo()-1; i++) {
-			for(int j = 0; j < tablero.obtenerLargo(); j++) {
+		for (int i = 0; i < tablero.obtenerLargoTablero()-1; i++) {
+			for(int j = 0; j < tablero.obtenerLargoTablero(); j++) {
 				if (tablero.sePuedeFusionar(tablero.obtenerFicha(i, j), tablero.obtenerFicha(i+1, j))){
 					sinCombinacionesVerticales = false;
 
@@ -22,8 +46,8 @@ public class Model {
 			}
 		}
 
-		for (int i = 0; i < tablero.obtenerLargo(); i++) {
-			for(int j = 0; j < tablero.obtenerLargo()-1; j++) {
+		for (int i = 0; i < tablero.obtenerLargoTablero(); i++) {
+			for(int j = 0; j < tablero.obtenerLargoTablero()-1; j++) {
 				if (tablero.sePuedeFusionar(tablero.obtenerFicha(i, j), tablero.obtenerFicha(i, j+1))){
 					sinCombinacionesHorizontales = false;
 
--- src/juego/Tablero.java
+++ src/juego/Tablero.java
@@ -1,52 +1,174 @@
 package juego;
 
+import java.util.Random;
+
 public class Tablero {
 	private int cantidadDeFichas;
+	private Ficha[][] matriz;
 
 	public Tablero() {
-		this.cantidadDeFichas = 0;
+		this.cantidadDeFichas = 2;
+		this.matriz = new Ficha[4][4];
 	}
 	
 	public void moverDerecha() {
-		
+		//recorremos las filas de abajo para arriba y las columnas de derecha a izquierda
+		for(int fila = 0; fila < obtenerLargoTablero(); fila++) {
+			
+			for(int col = obtenerLargoTablero()-2; col >= 0; col --) {
+				
+				if(matriz[fila][col] != null) {
+					
+					if(matriz[fila][col + 1] == null) {
+						matriz[fila][col + 1] = matriz[fila][col];
+						matriz[fila][col] = null;
+					}
+					else {
+						combinarFichas(fila, col, fila, col + 1);
+					}
+				}
+			}
+		}
 	}
+	
 	public void moverIzquierda() {
-		
+		//recorremos las filas de abajo para arriba y las columnas de izquierda a derecha
+		for(int fila = 0; fila < obtenerLargoTablero(); fila++) {
+			
+			for(int col = 1; col < obtenerLargoTablero(); col++) {
+				
+				if(matriz[fila][col] != null) {
+					
+					if(matriz[fila][col - 1] == null) {
+						matriz[fila][col - 1] = matriz[fila][col];
+						matriz[fila][col] = null;
+					}
+					else {
+						combinarFichas(fila, col, fila, col - 1);
+					}
+				}
+			}
+		}
 	}
+	
 	public void moverArriba() {
-		
+		//recorremos las filas desde abajo hacia arriba y las columnas desde izquierda a derecha
+		for(int fila = 1; fila < obtenerLargoTablero(); fila++) {
+			
+			for(int col = 0; col < obtenerLargoTablero(); col++) {
+				
+				if(matriz[fila][col] != null) {
+					
+					if(matriz[fila-1][col] == null) {
+						matriz[fila - 1][col] = matriz[fila][col];
+						matriz[fila][col] = null;
+					}
+					else {
+						combinarFichas(fila, col, fila - 1, col);
+					}
+				}
+			}
+		}
 	}
+	
 	public void moverAbajo() {
-		
+		//recorremos las filas desde arriba hacia abajo y las columnas desde izquierda a derecha
+		for(int fila = obtenerLargoTablero() - 2; fila >= 0; fila--) {
+			
+			for(int col = 0; col < obtenerLargoTablero(); col ++) {
+				
+				if(matriz[fila][col] != null) {
+					
+					if (matriz[fila + 1][col] == null) {
+						matriz[fila + 1][col] = matriz[fila][col];
+						matriz[fila][col]= null;
+					}
+					else {
+						combinarFichas(fila, col, fila + 1, col);
+					}
+				}
+			}
+		}
 	}
 	
-	private boolean validarPosicion() {
-		return false;
+	private boolean validarSiHayBorde(int fila, int columna, String direccion) {
+		//traemos el largo de fila y columna
+		int maximoMatriz = matriz.length - 1;
+		
+		if(direccion.equals("arriba") && fila == 0) {
+			return true;
+		}
+		else if(direccion.equals("abajo") && fila == maximoMatriz) {
+			return true;
+		}
+		else if(direccion.equals("izquierda") && columna == 0) {
+			return true;
+		}
+		else if(direccion.equals("derecha") && columna == maximoMatriz) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
-	public int obtenerLargo() {
-		
-		return 0;
+	public int obtenerLargoTablero() {
+		return matriz.length;
 	}
 	
-	public void combinarFichas() {
+	public boolean posicionLibre(int nFila, int nCol) {
+		if(matriz[nFila][nCol] == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void combinarFichas(int filaFichaOrigen, int columFichaOrigen, int  filaFichaDestino, int columFichaDestino) {
+		//usamos las coordenadas de los parametros para saber el valor de las fichas
+		Ficha fichaOrigen = matriz[filaFichaOrigen][columFichaOrigen];
+		Ficha fichaDestino = matriz[filaFichaDestino][columFichaDestino];
 		
+		if(sePuedeFusionar(fichaDestino, fichaOrigen)) {
+			int nuevoValorFicha = fichaDestino.getValor() + fichaOrigen.getValor();
+			fichaDestino.setValor(nuevoValorFicha);
+			
+			matriz[filaFichaOrigen][columFichaOrigen] = null;
+			
+			this.cantidadDeFichas--;
+		}
 	}
 	
 	public Ficha obtenerFicha(int i, int j) {
-		return null;
+		return matriz[i][j];
 	}
 	
-	public int cantidadDeFichas() {
+	public int cantidadDeFichasPresentes() {
 		return this.cantidadDeFichas;
 	}
 
-	public void agregarFicha() {
-		
+	
+	public void agregarFicha(String direccion, int siguienteValor) {
+		Random random = new Random();
 	}
 
-	public boolean sePuedeFusionar(Ficha obtenerFicha1, Ficha obtenerFicha2) {
-		return false;
+	public boolean sePuedeFusionar(Ficha ficha1, Ficha ficha2) {
+		if(ficha1 == null | ficha2 == null) {
+			return false;
+		}
+		
+		int valor1 = ficha1.getValor();
+		int valor2 = ficha2.getValor();
+		
+		if((valor1 == 1 && valor2 == 2) || (valor1 == 2 && valor2 == 1)){
+			return true;
+		}
+		else if(valor1 == valor2 && valor1 >= 3) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
